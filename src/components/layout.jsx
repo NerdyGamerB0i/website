c0dd78fef0674c16ebfd0dcfd20bb363c6303980
Cloudburst <18114966+C10udburst@users.noreply.github.com>
2022-08-28 18:53:55 +0200

    add git mirror

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -2,6 +2,7 @@ import React from "react"
 
 import Navbar from "./navbar"
 import Drawer from "./drawer"
+import Footer from "./footer"
 
 import './layout.css'
 
@@ -13,18 +14,7 @@ const Layout = ({ children }) => (
             <div className="pb-10 w-full text-base-content grow">
                 {children}
             </div>
-            <footer class="footer footer-center p-10 mt-15 bg-base-200 text-base-content rounded gap-2">
-                <div class="grid grid-flow-col gap-4">
-                    <a class="link link-hover" href="https://discord.gg/5Hus6fM">Contact</a>
-                    <a class="link link-hover" href="https://github.com/recloudstream/">Github</a>
-                    <a class="link link-hover" href="https://discord.gg/5Hus6fM">Discord</a>
-                    <a class="link link-hover" href="https://matrix.to/#/#lagrapps:matrix.org">Matrix</a>
-                    <a class="link link-hover" onClick={() => {window.open("https://www.youtube.com/watch?v=f-UIBVsRSDQ")}}>DMCA</a>
-                </div>
-                <div>
-                    <p>Website by <a className="link" href="https://wilkins.ml" target="_blank">Cloudburst</a></p>
-                </div>
-            </footer>
+            <Footer />
         </div>
         <div className="drawer-side">
             <label for="drawer" className="drawer-overlay backdrop-blur-sm"></label>
