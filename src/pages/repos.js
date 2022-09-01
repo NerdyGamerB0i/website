a0e92120b52aabf54ed8c646665eef65e8834af2
Cloudburst <18114966+C10udburst@users.noreply.github.com>
2022-09-01 09:56:43 +0200

    add a warning

--- src/pages/repos.js
+++ src/pages/repos.js
@@ -4,6 +4,8 @@ import Layout from "../components/layout"
 import RepoCard from "../components/cards/repo"
 import bgImage from "../media/phones.png"
 
+import { TiWarning } from "react-icons/ti";
+
 const IndexPage = () => {
     const [repos, setRepos] = useState([]);
     
@@ -17,6 +19,15 @@ const IndexPage = () => {
 
     return <Layout>
         <div className="flex items-center w-full flex-col">
+            <div className="alert alert-warning shadow-lg w-full mx-10 md:w-2/3 mb-5">
+                <div>
+                    <TiWarning className="stroke-current flex-shrink-0 h-6 w-6"/>
+                    <div>
+                        <h3 className="font-bold text-xl">Keep in mind that the extensions can execute arbitrary code inside the app.</h3>
+                        <span className="text-xs">This means you should treat them with the same level of scrutiny you treat any apps. Extensions can also read all of the Cloudstream's data.</span>
+                    </div>
+                </div>
+            </div>
             {repos &&
                 repos.map((it, index) => <RepoCard url={it} key={index} isFirst={index===0}/>)
             }
