4b16174ceae723b8634c937e7c2642fa4a5d8d56
Cloudburst <18114966+C10udburst@users.noreply.github.com>
2022-09-01 10:13:02 +0200

    add canonical urls

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,12 +4,7 @@ module.exports = {
     "gatsby-plugin-postcss",
     'gatsby-plugin-preact',
     'gatsby-plugin-sitemap',
-    {
-      resolve: 'gatsby-plugin-robots-txt',
-      options: {
-        policy: [{userAgent: '*', allow: '/'}]
-      }
-    },
+    { resolve: 'gatsby-plugin-robots-txt', options: { policy: [{userAgent: '*', allow: '/'}] } },
     {
       resolve: 'gatsby-source-filesystem',
       options: {
@@ -31,17 +26,9 @@ module.exports = {
         ]
       }
     },
-    {
-      resolve: 'gatsby-plugin-html-attributes',
-      options: {
-        lang: 'en',
-        'data-theme': 'dark'
-      }
-    },
+    { resolve: 'gatsby-plugin-html-attributes', options: { lang: 'en', 'data-theme': 'dark' } },
     { resolve: 'gatsby-plugin-nprogress', options: { color: '#6419E6' } },
-    {
-      resolve: 'gatsby-plugin-manifest',
-      options: require("./manifest")
-    },
+    { resolve: 'gatsby-plugin-manifest', options: require("./manifest") },
+    { resolve: 'gatsby-plugin-canonical-urls', options: { siteUrl: require("./metadata").siteUrl } },
   ]
 };
