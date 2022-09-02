af4ca2263294c519842061f7601289c136e4ed1a
Cloudburst <18114966+C10udburst@users.noreply.github.com>
2022-09-02 09:31:42 +0200

    fix hero

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,7 +1,9 @@
 import React from "react"
 
+import "./hero.css"
+
 const Hero = ({bg, children}) => (
-    <div className="hero min-h-screen" style={{backgroundImage: `url(${bg})`}}>
+    <div className="hero hero-full min-h-screen" style={{backgroundImage: `url(${bg})`}}>
     <div className="hero-overlay bg-opacity-60" />
     <div className="hero-content text-center text-neutral-content">
         <div className="max-w-md">
